c35e1f93c8fe275cf9f19c4b7cfb3d353a972f24
omeryzr <omeryzr_@hotmail.com>
2018-07-10 21:45:28 +0300

    create WepApp
    Bootstrap With Spring Boot and usage Thymeleaf

---
+++ src/main/java/com/bootstrapwithspringboot/webapp/contoller/WebAppContoller.java
@@ -0,0 +1,29 @@
+package com.bootstrapwithspringboot.webapp.contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
+
+@Controller
+public class WebAppContoller {
+    private String appMode;
+
+    @Autowired
+    public WebAppContoller(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "@omeryazir");
+        model.addAttribute("projectname", "WebApp");
+
+        model.addAttribute("mode", appMode);
+
+        return "index";
+    }
+}
